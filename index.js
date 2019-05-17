7acd63baf9c4cc5170c5e6b2d8f9cb0f0a343077
Jorge Luis Serna Velasquez <jorgelserve@icloud.com>
2019-05-17 03:33:12 -0500

    reparando un priblema

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ const express  = require('express')
 const app = express()
 
 
+var id = 0
 
 app.get('/', (req, res) => {
-	res.send('hola mundo')
+	if (!id) {
+		res.send('No hay equipos prestados')
+	} else {
+		res.send(`${id} presto un equipo`)
+	}
 })
 
 app.get('/a/:id', (req, res) => {
 	res.send('hola mundo')
-	console.log(`nuevo prestamo por parte del id ${req.params.id}`)
+	if (id == 0) {
+		id = req.params.id
+		console.log(`nuevo prestamo por parte del id ${req.params.id}`)
+	} else if (id == req.params.id){
+		id = 0
+		console.log(`${req.params.id} devolvio equipo`)
+	}
+	
 })
 
 var port = process.env.PORT || 3000
