9c6b7911b27f683b2343ca1bafe1d95cac532c6e
Jorge Luis Serna Velasquez <jorgelserve@icloud.com>
2019-05-30 15:15:53 -0500

    actualizando nombre

--- public/index.js
+++ public/index.js
@@ -51155,7 +51155,7 @@ const yo = require('yo-yo')
 module.exports = yo`<div>
 <nav class="nav-extended indigo accent-2">
     <div class="nav-wrapper">
-      <a href="/" class="brand-logo">Logo</a>
+      <a href="/" class="brand-logo">Vinariam</a>
     </div>
   </nav>
 </div>`
