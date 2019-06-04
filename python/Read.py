e4340700c224f9322251785e00714322f89b1c4e
jsanch90 <jsanch90@github.com>
2019-06-04 15:25:39 -0500

    Version estable de progama de python

--- python/Read.py
+++ python/Read.py
@@ -2,8 +2,11 @@
 
 import requests
 import serial
-arduino = serial.Serial('/dev/cu.usbmodem14401', baudrate=9600, timeout=1.0)
+arduino = serial.Serial('/dev/ttyACM0', baudrate=9600, timeout=1.0)
 
 while True:
     line = arduino.readline()
-    print(line)
+    line = line[0:-1]
+    if len(line) > 0:
+        print(line[0:-1])
+        r = requests.get('localhost:3000/api/' + line)
