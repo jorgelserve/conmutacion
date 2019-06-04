15b4eac66c0986ba00bcf993ce6880fba446bf78
jsanch90 <jsanch90@github.com>
2019-06-04 18:13:44 -0500

    problema que no permitia el request solucionado

--- python/Read.py
+++ python/Read.py
@@ -6,7 +6,6 @@ arduino = serial.Serial('/dev/ttyACM0', baudrate=9600, timeout=1.0)
 
 while True:
     line = arduino.readline()
-    line = line[0:-1]
     if len(line) > 0:
         print(line)
-        r = requests.get("http://vinariam.herokuapp.com/api/" + str(line))
+        r = requests.get("http://vinariam.herokuapp.com/api/" + str(line[0:-2]))
